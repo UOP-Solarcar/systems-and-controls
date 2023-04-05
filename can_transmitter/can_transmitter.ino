35c60f385ca6100607786ecb681feeb0d5c8c2f6
MiguelX413 <lbpgaming27@gmail.com>
2023-04-04 18:43:20 -0700

    Make cooler

--- can_transmitter/can_transmitter.ino
+++ can_transmitter/can_transmitter.ino
@@ -1,39 +1,27 @@
-#include <SPI.h>          //Library for using SPI Communication 
-#include <mcp2515.h>      //Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
- 
+#include <SPI.h>     //Library for using SPI Communication
+#include <mcp2515.h> //Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
 
- 
-struct can_frame canMsg;
- 
 MCP2515 mcp2515(10);
 
- 
- 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  SPI.begin();               //Begins SPI communication
- 
+  SPI.begin(); // Begins SPI communication
+
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
+  mcp2515.setBitrate(CAN_500KBPS,
+                     MCP_8MHZ); // Sets CAN at speed 500KBPS and Clock 8MHz
   mcp2515.setNormalMode();
 }
- 
-// uint8_t xd = 0;
-void loop()
-{
 
-  canMsg.can_id  = 0x022;           //CAN id as 0x036
-  canMsg.can_dlc = 8;               //CAN data length as 8
-  canMsg.data[0] = 0x01;               //Update humidity value in [0]
-  canMsg.data[1] = 0x02;               //Update temperature value in [1]
-  canMsg.data[2] = 0x03;            //Rest all with 0
-  canMsg.data[3] = 0x04;
-  canMsg.data[4] = 0x05;
-  canMsg.data[5] = 0x6B;
-  canMsg.data[6] = 0x07;
-  canMsg.data[7] = 0x00;
-  
-  mcp2515.sendMessage(&canMsg);     //Sends the CAN message
+uint8_t i = 0;
+void loop() {
+  can_frame can_msg;
+  can_msg.can_id = 0x022; // CAN id as 0x036
+  can_msg.can_dlc = 8;    // CAN data length as 8
+  for (auto &datum : can_msg.data) {
+    datum = i++;
+  }
+
+  mcp2515.sendMessage(&can_msg); // Sends the CAN message
   delay(100);
 }
