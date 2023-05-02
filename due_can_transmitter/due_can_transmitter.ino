14edc85fdd77dba0d021496a8e0ddb23693f3100
MiguelX413 <lbpgaming27@gmail.com>
2023-05-01 18:20:36 -0700

    Create due_can_transmitter

---
+++ due_can_transmitter/due_can_transmitter.ino
@@ -0,0 +1,16 @@
+#include <due_can.h>
+
+void setup() { Can0.init(CAN_BPS_500K); }
+
+uint8_t num = 0;
+void loop() {
+  CAN_FRAME frame;
+  frame.id = 0x13;
+  frame.length = 8;
+  for (size_t i = 0; i < frame.length; i++) {
+    frame.data.bytes[i] = num++;
+  }
+
+  Can0.sendFrame(frame);
+  delay(100);
+}
