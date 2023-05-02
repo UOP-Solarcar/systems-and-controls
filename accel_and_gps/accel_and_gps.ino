a16a5e90ac8f95b67f53553c319b04b507cb595f
MiguelX413 <lbpgaming27@gmail.com>
2023-05-01 23:24:34 -0700

    Make GPS msg fit on one line

--- accel_and_gps/accel_and_gps.ino
+++ accel_and_gps/accel_and_gps.ino
@@ -42,42 +42,42 @@ void loop() {
   Serial.print(':');
   Serial.print(GPS.seconds, DEC);
   Serial.print('.');
-  Serial.println(GPS.milliseconds);
+  Serial.print(GPS.milliseconds);
 
-  Serial.print("Date: ");
+  Serial.print(" | Date: ");
   Serial.print(GPS.day, DEC);
   Serial.print('/');
   Serial.print(GPS.month, DEC);
   Serial.print("/20");
-  Serial.println(GPS.year, DEC);
+  Serial.print(GPS.year, DEC);
 
-  Serial.print("Fix: ");
+  Serial.print(" | Fix: ");
   Serial.print(GPS.fix);
   Serial.print(" quality: ");
-  Serial.println(GPS.fixquality);
-  Serial.print("Satellites: ");
-  Serial.println(GPS.satellites);
+  Serial.print(GPS.fixquality);
+  Serial.print(" | Satellites: ");
+  Serial.print(GPS.satellites);
 
   if (GPS.fix) {
-    Serial.print("Location: ");
+    Serial.print(" | Location: ");
     Serial.print(GPS.latitude, 4);
     Serial.print(GPS.lat);
     Serial.print(", ");
     Serial.print(GPS.longitude, 4);
-    Serial.println(GPS.lon);
-    Serial.print("Google Maps location: ");
+    Serial.print(GPS.lon);
+    Serial.print(" | Google Maps location: ");
     Serial.print(GPS.latitudeDegrees, 4);
     Serial.print(", ");
-    Serial.println(GPS.longitudeDegrees, 4);
+    Serial.print(GPS.longitudeDegrees, 4);
 
-    Serial.print("Speed (knots): ");
-    Serial.println(GPS.speed);
-    Serial.print("Heading: ");
-    Serial.println(GPS.angle);
-    Serial.print("Altitude: ");
-    Serial.println(GPS.altitude);
+    Serial.print(" | Speed (knots): ");
+    Serial.print(GPS.speed);
+    Serial.print(" | Heading: ");
+    Serial.print(GPS.angle);
+    Serial.print(" | Altitude: ");
+    Serial.print(GPS.altitude);
   }
-  Serial.println("-------------------------------------");
+  Serial.println();
 
   // Accel
   Wire.beginTransmission(MPU_ADDR);
