9c728aaaf54260570a47a6811fd60f38e4429565
MiguelX413 <lbpgaming27@gmail.com>
2023-06-06 11:03:58 -0700

    Add arduino_voltmeter

---
+++ arduino_voltmeter/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+int ref_read = 1024;
+
+const uint8_t ANALOG_IN_PIN = 0;
+
+const double ref_v = 4.2;
+
+void setup() {
+  // Setup Serial Monitor
+  Serial.begin(19200);
+  Serial.println("DC Voltage Test");
+  Serial.print("Supply ");
+  Serial.print(ref_v);
+  Serial.println("v for Callibration");
+  for (size_t i = 0; i < 5; i++) {
+    Serial.println(5 - i);
+    delay(1000);
+  };
+  ref_read = analogRead(ANALOG_IN_PIN);
+  Serial.print("Callibrated as ");
+  Serial.println(ref_read);
+}
+
+void loop() {
+  double v = analogRead(ANALOG_IN_PIN) / ((ref_read / ref_v));
+
+  // Print results to Serial Monitor to 2 decimal places
+  Serial.print("Input Voltage = ");
+  Serial.println(v);
+
+  // Short delay
+  delay(250);
+}
