7d5360d644b332c1857bae35dc2d5c3a6a8fbccf
kyasig <kevinyang177013@gmail.com>
2024-02-21 20:43:11 -0800

    reads digital signal

--- hallEffectSensor/src/main.cpp
+++ hallEffectSensor/src/main.cpp
@@ -3,33 +3,28 @@
 const int Pin1 = A1;
 const int Pin2 = A2;
 const int Pin3 = A3;
-const int Pin4 = A4;
 int reading1;
 int reading2;
 int reading3;
-int reading4;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(Pin1, INPUT);
   pinMode(Pin2, INPUT);
   pinMode(Pin3, INPUT);
-  pinMode(Pin4, INPUT);
 }
 
 void loop() {
-  reading1 = analogRead(Pin1);
-  reading2 = analogRead(Pin2);
-  reading3 = analogRead(Pin3);
-  reading4 = analogRead(Pin4);
+  reading1 = digitalRead(Pin1);
+  reading2 = digitalRead(Pin2);
+  reading3 = digitalRead(Pin3);
   Serial.print("Readings ");
   Serial.print(reading1);
   Serial.print(", ");
   Serial.print(reading2);
   Serial.print(", ");
   Serial.print(reading3);
-  Serial.print(", ");
-  Serial.println(reading4);
   delay(20);
+  Serial.print('\n');
 }
 
