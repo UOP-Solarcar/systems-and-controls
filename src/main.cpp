d65ad32c27becc7256013c491e311fa20f922088
kyasig <kevinyang177013@gmail.com>
2024-07-07 00:47:08 -0700

    light signal and headlight code

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,100 @@
 #include <Arduino.h>
 
-const uint8_t RELAY_PIN_1 = 2;
-const uint8_t RELAY_PIN_2 = 3;
-const uint8_t RELAY_PIN_3 = 4;
-const uint8_t RELAY_PIN_4 = 5;
+const uint8_t RIGHT_PIN = 2;
+const uint8_t LEFT_PIN = 3;
+const uint8_t HEADLIGHT_PIN = 4;
 const uint8_t RELAY_PIN_5 = 6;
 const uint8_t RELAY_PIN_6 = 7;
 const uint8_t RELAY_PIN_7 = 8;
 const uint8_t RELAY_PIN_8 = 9;
 
+enum SignalState {
+  HAZARD,
+  TURNRIGHTBRAKE,
+  TURNLEFTBRAKE,
+  TURNLEFT,
+  TURNRIGHT,
+  DEF
+};
+
+enum HeadlightState {
+  ON,
+  OFF
+};
+
+SignalState state = TURNLEFTBRAKE;
+HeadlightState Hlstate = OFF;
+
+void reset(){
+    // Initialize relay pins as off
+    digitalWrite(RIGHT_PIN, HIGH);
+    digitalWrite(LEFT_PIN, HIGH);
+    //digitalWrite(HEADLIGHT_PIN, HIGH);
+    digitalWrite(RELAY_PIN_4, HIGH);
+    digitalWrite(RELAY_PIN_5, HIGH);
+    digitalWrite(RELAY_PIN_6, HIGH);
+    digitalWrite(RELAY_PIN_7, HIGH);
+    digitalWrite(RELAY_PIN_8, HIGH);
+}
 void setup() {
     Serial.begin(9600);
-    
-    pinMode(RELAY_PIN_1, OUTPUT);
-    pinMode(RELAY_PIN_2, OUTPUT);
-    pinMode(RELAY_PIN_3, OUTPUT);
+
+    pinMode(RIGHT_PIN, OUTPUT);
+    pinMode(LEFT_PIN, OUTPUT);
+    pinMode(HEADLIGHT_PIN, OUTPUT);
     pinMode(RELAY_PIN_4, OUTPUT);
     pinMode(RELAY_PIN_5, OUTPUT);
     pinMode(RELAY_PIN_6, OUTPUT);
     pinMode(RELAY_PIN_7, OUTPUT);
     pinMode(RELAY_PIN_8, OUTPUT);
 
-    // Initialize relay pins as off
-    digitalWrite(RELAY_PIN_1, HIGH);
-    digitalWrite(RELAY_PIN_2, HIGH);
-    digitalWrite(RELAY_PIN_3, HIGH);
-    digitalWrite(RELAY_PIN_4, HIGH);
-    digitalWrite(RELAY_PIN_5, HIGH);
-    digitalWrite(RELAY_PIN_6, HIGH);
-    digitalWrite(RELAY_PIN_7, HIGH);
-    digitalWrite(RELAY_PIN_8, HIGH);
-
+    reset();
 }
 
+void flash(uint8_t pin, int wait = 750){
+    digitalWrite(pin, LOW);
+    delay(wait);
+    digitalWrite(pin, HIGH);
+    delay(wait);
+}
+
+void flash2(uint8_t pin0, uint8_t pin1, int wait = 750){
+    digitalWrite(pin0, LOW);
+    digitalWrite(pin1, LOW);
+    delay(wait);
+    digitalWrite(pin0, HIGH);
+    digitalWrite(pin1, HIGH);
+    delay(wait);
+}
 void loop() {
-    // Cycle through relays
-    Serial.println("Turning on relay 1");
-    digitalWrite(RELAY_PIN_1, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 1");
-    digitalWrite(RELAY_PIN_1, HIGH);
-
-    Serial.println("Turning on relay 2");
-    digitalWrite(RELAY_PIN_2, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 2");
-    digitalWrite(RELAY_PIN_2, HIGH);
-
-    Serial.println("Turning on relay 3");
-    digitalWrite(RELAY_PIN_3, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 3");
-    digitalWrite(RELAY_PIN_3, HIGH);
-
-    Serial.println("Turning on relay 4");
-    digitalWrite(RELAY_PIN_4, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 4");
-    digitalWrite(RELAY_PIN_4, HIGH);
-
-    Serial.println("Turning on relay 5");
-    digitalWrite(RELAY_PIN_5, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 5");
-    digitalWrite(RELAY_PIN_5, HIGH);
-
-    Serial.println("Turning on relay 6");
-    digitalWrite(RELAY_PIN_6, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 6");
-    digitalWrite(RELAY_PIN_6, HIGH);
-
-    Serial.println("Turning on relay 7");
-    digitalWrite(RELAY_PIN_7, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 7");
-    digitalWrite(RELAY_PIN_7, HIGH);
-
-    Serial.println("Turning on relay 8");
-    digitalWrite(RELAY_PIN_8, LOW);
-    delay(1000);
-    Serial.println("Turning off relay 8");
-    digitalWrite(RELAY_PIN_8, HIGH);
-
+    switch (state) {
+      case HAZARD:
+        flash2(RIGHT_PIN, LEFT_PIN);
+        break;
+      case TURNRIGHTBRAKE:
+        digitalWrite(LEFT_PIN, LOW);
+        flash(RIGHT_PIN);
+        break;
+      case TURNLEFTBRAKE:
+        digitalWrite(RIGHT_PIN, LOW);
+        flash(LEFT_PIN);
+        break;
+      case TURNLEFT:
+        digitalWrite(LEFT_PIN, LOW);
+        break;
+      case TURNRIGHT:
+        digitalWrite(RIGHT_PIN, LOW);
+        break;
+      case DEF:
+        reset();
+        break;
+    }
+    switch (Hlstate){
+      case ON:
+        digitalWrite(HEADLIGHT_PIN, LOW);
+        break;
+      case OFF:
+        digitalWrite(HEADLIGHT_PIN, HIGH);
+        break;
+    }
 }
