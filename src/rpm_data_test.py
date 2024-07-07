e1398cb2f0d2c180b2745ce6a2febb9c2213752a
Pranav Jayakumar <p_jayakumar@u.pacific.edu>
2024-07-07 04:15:10 -0700

    successfully created pygame script to display rpm data test stream to display

---
+++ src/rpm_data_test.py
@@ -0,0 +1,14 @@
+file_path = '/Users/pranavjay/Downloads/rpm'
+
+def print_rpm_data():
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                print(line.strip())
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except Exception as e:
+        print(f"Error reading from file: {e}")
+
+if __name__ == "__main__":
+    print_rpm_data()
