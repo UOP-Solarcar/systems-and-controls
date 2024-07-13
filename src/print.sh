b3815024082d92c68dadcd1ca80f08a08b7fd78d
Pranav Jayakumar <p_jayakumar@u.pacific.edu>
2024-07-12 23:22:42 -0500

    sussy

--- src/print.sh
+++ src/print.sh
@@ -1,9 +1,17 @@
-#! /usr/bin/env sh
+#!/usr/bin/env sh
 
-if [-z "$1"]; then
+if [ -z "$1" ]; then
   echo "usage $0 filename"
   exit 1
 fi
+
+if [ ! -r "$1" ]; then
+  echo "File $1 does not exist or is not readable."
+  exit 1
+fi
+
+echo "Processing file: $1"
+
 while IFS= read -r line; do
-  echo $line
+  echo "$line"
 done < "$1"
