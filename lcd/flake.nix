3bbe23e832aa1f0dcfa10fc1fe47ae293e602de5
pranav-jay26 <p_jayakumar@u.pacific.edu>
2025-01-01 17:08:17 -0800

    package lcd as nix flake

---
+++ lcd/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Solar Car LCD Display";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+      in
+      {
+        packages.default = python.pkgs.buildPythonApplication {
+          pname = "lcd";
+          version = "0.1.0";
+          format = "setuptools";
+
+          src = builtins.path {
+            path = ./.;
+            name = "lcd-source";
+          };
+
+          propagatedBuildInputs = with python.pkgs; [
+            pyqt6
+          ];
+
+          nativeBuildInputs = with python.pkgs; [
+            setuptools
+          ];
+
+          buildInputs = with pkgs; [
+            qt6.full
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python
+            python.pkgs.pyqt6
+            qt6.full
+          ];
+        };
+      }
+    );
+} 
---
+++ lcd/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lcd",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "PyQt6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "lcd=lcd.__main__:main",
+        ],
+    },
+) 
