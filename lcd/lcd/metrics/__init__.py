067c2f983a716c8dbd2422c54e3ab5105e42ee14
pranav-jay26 <p_jayakumar@u.pacific.edu>
2025-01-10 21:28:40 -0800

    clean up imports

--- lcd/lcd/metrics/__init__.py
+++ lcd/lcd/metrics/__init__.py
@@ -1,5 +1,5 @@
 from .calculations import read_metrics_from_stdin, calculate_speed, kwh_per_100_km
-from .parser import parse_line, parse_motor_controller, parse_bms
+from .parser import parse_line
 import sys
 import queue
 
