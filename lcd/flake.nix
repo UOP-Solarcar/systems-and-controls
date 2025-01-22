0241c82bf93003b54b677067050c835f9decbbcf
kyasig <kevinyang177013@gmail.com>
2025-01-22 09:42:10 -0800

    move flake.nix and flake.lock to lcd directory

--- lcd/flake.nix
+++ lcd/flake.nix
@@ -6,11 +6,11 @@
     flake-utils.url = "github:numtide/flake-utils";
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    cargo-utils.url = "github:rust-lang/cargo";
-    cargo-utils.flake = false;
+    #cargo-utils.url = "github:rust-lang/cargo";
+    #cargo-utils.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, darwin, cargo-utils }:
+  outputs = { self, nixpkgs, flake-utils, darwin }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -22,13 +22,13 @@
           version = "0.1.0";
           format = "setuptools";
 
-          src = ./.;
+          src = self;
 
           propagatedBuildInputs = with python.pkgs; [
             pyqt6
             pyarrow
             pydantic
-            (pkgs.callPackage ./lcd-metrics-rs {
+            (pkgs.callPackage ../lcd-metrics-rs {
               inherit python pkgs;
             })
           ];
@@ -58,24 +58,6 @@
             maturin
           ];
 
-          shellHook = ''
-            echo "Setting up pre-commit hook for black..."
-            if [ ! -f .git/hooks/pre-commit ]; then
-              mkdir -p .git/hooks
-              cat > .git/hooks/pre-commit << 'EOF'
-#!/usr/bin/env bash
-set -e
-FILES=$(git diff --cached --name-only --diff-filter=d | grep -E '\.py$' || true)
-if [ -n "$FILES" ]; then
-  echo "Running black on Python files..."
-  black $FILES
-  git add $FILES
-fi
-EOF
-              chmod +x .git/hooks/pre-commit
-              echo "Pre-commit hook installed!"
-            fi
-          '';
         };
       }
     );
