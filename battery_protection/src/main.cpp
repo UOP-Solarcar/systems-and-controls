113d61fcbba7694a088dbaf2415ac3dbcdc56945
smcc977 <sammcc977@gmail.com>
2025-06-04 18:21:58 -0700

    adding can to bps

--- battery_protection/src/main.cpp
+++ battery_protection/src/main.cpp
@@ -1,4 +1,38 @@
 #include "Arduino.h"
+#include "../../bytetools/src/bytetools.cpp"
+#include "../../bytetools/src/bytetools.hpp"
+
+#if defined(RPI4B)
+#include <cstring>
+#include <iostream>
+#include <linux/can.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#else
+#include <Arduino.h>
+#include <SPI.h>
+#include <mcp2515.h> // (https://github.com/autowp/arduino-mcp2515/)
+#endif
+
+#if defined(RPI4B)
+typedef std::ostream Out;
+template <typename T> void print_n(T t, Out &out = std::cout) { out << t; }
+void print_n(uint8_t n, Out &out = std::cout) { out << (uint16_t)n; }
+void print_n(int8_t n, Out &out = std::cout) { out << (int16_t)n; }
+template <typename T> void print_hex(T t, Out &out = std::cout) {
+  out << std::hex << t << std::dec;
+}
+void println(Out &out = std::cout) { out << std::endl; }
+#else
+typedef Print Out;
+template <typename T> void print_n(T t, Out &out = Serial) { out.print(t); }
+template <typename T> void print_hex(T t, Out &out = Serial) {
+  out.print(t, HEX);
+}
+void println(Out &out = Serial) { out.println(); }
+#endif
 
 class Relay {
 
@@ -116,6 +150,92 @@ private:
   bool     _latched    = false;
 };
 
+/*
+Cases that trip bps:
+Estop
+overvoltage and overcurrent 
+100A
+-20A Charging current
+Any module over 4.2 volts
+Any module under 2.5 volts
+*/
+bool parse_frame(can_frame &frame, Out &out =
+#if defined(RPI4B)
+                                       std::cout
+#else
+                                       Serial
+#endif
+) {
+  switch (frame.can_id) {
+    case 0x6B0: {
+      int16_t pack_current = bytetools::int_bswap(*(int16_t *)&frame.data[0]);
+      uint16_t pack_inst_voltage =
+          bytetools::int_bswap(*(uint16_t *)&frame.data[2]);
+      uint8_t pack_soc = frame.data[4];
+      uint16_t relay_state = bytetools::int_bswap(*(uint16_t *)&frame.data[5]);
+      uint8_t checksum = frame.data[7];
+
+      if (pack_current < -20){
+        Serial.println("Exceeding Maximum Charging Current");
+        return true;
+      }
+      if (pack_current > 100){
+        Serial.println("Exceeding Maximum Discharge Current");
+        return true;
+      }
+    } return false;
+    case 0x6B1: {
+      uint16_t pack_dcl = bytetools::int_bswap(*(uint16_t *)&frame.data[0]),
+               pack_ccl = bytetools::int_bswap(*(uint16_t *)&frame.data[2]);
+      uint8_t high_temp = frame.data[4], low_temp = frame.data[5],
+              checksum = frame.data[7];
+    } return false;
+    case 0x6B2: {
+      uint16_t high_cell_voltage =
+          bytetools::int_bswap(*(uint16_t *)&frame.data[0]);
+      uint8_t high_cell_voltage_id = frame.data[2];
+      uint16_t low_cell_voltage =
+          bytetools::int_bswap(*(uint16_t *)&frame.data[3]);
+      uint8_t low_cell_voltage_id = frame.data[5], checksum = frame.data[6];
+      if (high_cell_voltage > 4.2){
+        Serial.println("Cell Overvoltage");
+        return true;
+      }
+      if (low_cell_voltage <= 2.5){
+        Serial.println("Cell Undervoltage");
+        return true;
+      }
+    } return false;
+    case 0x6B3: {
+      uint8_t high_temp = frame.data[0], high_thermistor_id = frame.data[1],
+              low_temp = frame.data[2], low_thermistor_id = frame.data[3],
+              avg_temp = frame.data[4], internal_temp = frame.data[5],
+              checksum = frame.data[6];
+    } return false;
+    case 0x6B4: {
+      uint8_t pack_health = frame.data[0];
+      uint16_t adaptive_total_capacity =
+                   bytetools::int_bswap(*(uint16_t *)&frame.data[3]),
+               input_supply_voltage =
+                   bytetools::int_bswap(*(uint16_t *)&frame.data[5]);
+      uint8_t checksum = frame.data[7];
+    } return false;
+    case 0x36: {
+      uint8_t cell_id = frame.data[0];
+      uint16_t instant_voltage =
+                   bytetools::int_bswap(*(uint16_t *)&frame.data[1]),
+               internal_resistance =
+                   bytetools::int_bswap(*(uint16_t *)&frame.data[3]),
+               open_voltage = frame.data[5];
+      uint8_t checksum = frame.data[7];
+    } return false;
+    default:
+      return false;
+  }
+}
+
+MCP2515 mcp2515(10);
+
 Relay contactorPrecharge(4, Relay::ACTIVE_LOW);
 Relay contactorPositive(5, Relay::ACTIVE_LOW);
 Relay contactorNegative(6, Relay::ACTIVE_LOW);
@@ -128,7 +248,6 @@ static unsigned long estopLowSince = 0;
 volatile bool estopTripped = false;
 bool bpsFaultState = false;
 bool onOffState {};
-
 
 
 void eStopISR() {
@@ -152,7 +271,14 @@ void setup() {
   
 
   Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Booted");
+  SPI.begin();
 
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS,
+                     MCP_8MHZ); // Sets CAN at speed 500KBPS and Clock 8MHz
+  mcp2515.setNormalMode();      // Sets CAN at normal mode
 }
 
 void loop() {
@@ -168,7 +294,11 @@ void loop() {
 
   static unsigned long ledT0 = 0;
   
-  
+  can_frame frame{};
+  if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK) {
+    bpsFaultState = parse_frame(frame);
+  }
+
   if (bpsFaultState) {
 
 
