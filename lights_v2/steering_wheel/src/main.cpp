53eda4422e20b35c0b5053dd3ad8990da0cdfce0
pranav-jay26 <p_jayakumar@u.pacific.edu>
2026-07-11 13:10:18 -0700

    Refactor the steering wheel-side logic
    Changes:
    
      - Track raw input state with a bitmask instead of a C-array of bools, debounce the entire mask at once instead of each individual button input
      - Declare millis() last change time variable as static to prevent it from being recreated on every function call and hogging memory
      - Populate CAN messages with a nested if statement in a for loop

--- lights_v2/steering_wheel/src/main.cpp
+++ lights_v2/steering_wheel/src/main.cpp
@@ -6,36 +6,38 @@
 
 MCP2515 mcp2515(10);
 
-const uint8_t PINS [] = {2, 3, 4, 5, 6, 7, 8, 9};
-const size_t NUMPINS = 8;
+constexpr uint8_t PINS [] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+constexpr size_t NUMPINS = 8;
+constexpr long unsigned int debounceTime = 20;
 
-bool state [8] = {false};
-bool sent [8] = {false};
-long unsigned int debounce_time = 20;
-long unsigned int debounce [NUMPINS] = {0};
+uint8_t inputState = 0;
+uint8_t lastRawState = 0;
+uint8_t stableState = 0;
+uint8_t lastStableState = 0;
 
 struct can_frame msg;
 
-uint8_t readPinValue(const uint8_t* arr, size_t size) {
-
-  for (size_t i = 0; i < size; i++)
-    if (digitalRead(arr[i]) == LOW ){
-      long unsigned int now = millis();
-      if (state[i] != true) {
-        state[i] = true;
-        debounce[i] = now;
-      }
-      if (now - debounce[i] > debounce_time && state[i]){ //&& sent[i] == false){
-        sent[i] = true;
-        return arr[i];
-      }
+inline void readPinMask(const uint8_t pins [], const size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    if (digitalRead(pins[i]) == LOW) {
+      inputState |= (1U << i);
     }
-    else {
-      state[i] = false;
-      //sent[i] = false;
-    }
+  }
+}
 
-  return 0;
+void updateInputs(const uint8_t pins [], const size_t size) {
+  readPinMask(pins, size);
+  static unsigned long lastChangeTime;
+  
+  // debounce the entire bitmask state
+  if (inputState != lastRawState) {
+    lastChangeTime = millis();
+    lastRawState = inputState;
+  }
+
+  if (millis() - lastChangeTime > debounceTime) {
+    stableState = inputState;
+  }
 }
 
 void setup() {
@@ -48,7 +50,7 @@ void setup() {
   msg.can_id  = 0x100;
   msg.can_dlc = 8;
 
- for (size_t i = 0; i < NUMPINS; i++) {
+  for (size_t i = 0; i < NUMPINS; i++) {
     msg.data[i] = 0x00;
   }
 
@@ -58,38 +60,26 @@ void setup() {
 
 }
 
+void loop () {
 
-void loop() {
+  updateInputs(PINS, NUMPINS);
 
-  uint8_t buttonPressed = readPinValue(PINS, NUMPINS);
-
-  msg.can_id  = 0x100;
+  msg.can_id = 0x100;
   msg.can_dlc = 8;
+
+  uint8_t pressed = stableState & ~lastStableState;
 
   for (size_t i = 0; i < NUMPINS; i++) {
     msg.data[i] = 0x00;
   }
 
-  if (buttonPressed != 0) {
-    switch(buttonPressed) {
-      case 2: msg.data[0] = 0x02; break;
-      case 3: msg.data[1] = 0x03; break;
-      case 4: msg.data[2] = 0x04; break;
-      case 5: msg.data[3] = 0x05; break;
-      case 6: msg.data[4] = 0x06; break;
-      case 7: msg.data[5] = 0x07; break;
-      case 8: msg.data[6] = 0x08; break;
-      case 9: msg.data[7] = 0x09; break;
-      default: break;
+  for (size_t i = 0; i < NUMPINS; i++) {
+    if (pressed & (1U << i)) {
+      msg.data[i] = i+2;
     }
-
-    Serial.print("Button pressed on pin ");
-    Serial.print(buttonPressed);
-    Serial.println();
   }
 
   mcp2515.sendMessage(&msg);
-
+  lastStableState = stableState;
 }
-
 
